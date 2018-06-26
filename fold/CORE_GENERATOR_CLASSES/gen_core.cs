265420580087acb85c9504f9af06a4f483d88d40
Philipp Dominic Siedler <p.d.siedler@gmail.com>
2018-06-26 16:21:50 +0100

    change

---
+++ fold/CORE_GENERATOR_CLASSES/gen_core.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Linq;
+using System.Web.Script.Serialization;
+using Grasshopper;
+using System.IdentityModel;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace core_generator
+{
+    class gen_core
+    {
+        private int core_min_width;
+        private int core_min_height;
+        public DataTree<Rectangle3d> core;
+        public List<Point3d> locations;
+        public DataTree<int> values;
+        private Rectangle3d skin;
+        private int max_core_count;
+        private double efficiency;
+        private double deviation;
+
+        public gen_core(int core_min_width, int core_min_height, ref Rectangle3d skin, int max_core_count, double efficiency, double deviation)
+        {
+            this.core_min_width = core_min_width;
+            this.core_min_height = core_min_height;
+            this.skin = skin;
+            this.max_core_count = max_core_count;
+            this.efficiency = efficiency;
+            this.deviation = deviation;
+        }
+
+        public void compute()
+        {
+            create_locations();
+
+        }
+
+        public void create_locations()
+        {
+            for (int skin_width = 0; skin_width < this.skin.Width; skin_width++)
+            {
+                for (int skin_height = 0; skin_height < this.skin.Height; skin_height++)
+                {
+                    this.locations.Add(new Point3d(skin_width, skin_height, 0));
+                }
+            }
+        }
+
+        public void create_cores()
+        {
+            double min_core_area = (this.skin.Width * this.skin.Height) * this.efficiency;
+            double remaining_core_area = min_core_area;
+            if(this.max_core_count > 0)
+            {
+                double area = this.core_min_width * this.core_min_height;
+                remaining_core_area = min_core_area - (area * (this.max_core_count - 1));
+            }
+
+            List<Rectangle3d> cores = new List<Rectangle3d>();
+            for (int core_width = this.core_min_width; core_width <= this.skin.Width; core_width++)
+            {
+                for (int core_height = this.core_min_height; core_height <= this.skin.Height; core_height++)
+                {
+                    if(min_core_area <= core_width * core_height && remaining_core_area >= core_width * core_height)
+                    {
+                        cores.Add(new Rectangle3d(Plane.WorldXY, core_width, core_height));
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ fold/CORE_GENERATOR_CLASSES/gen_skin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System.Linq;
+using System.Web.Script.Serialization;
+using Grasshopper;
+using System.IdentityModel;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace core_generator
+{
+    class gen_skin
+    {
+        public List<Rectangle3d> skin;
+        private int max_skin_width;
+        private int max_skin_height;
+        private bool variety;
+        private double deviation;
+        private int area;
+
+        public gen_skin(int max_skin_width, int max_skin_height, bool variety, double deviation)
+        {
+            this.max_skin_width = max_skin_width;
+            this.max_skin_height = max_skin_height;
+            this.variety = variety;
+            this.deviation = deviation;
+            this.area = max_skin_width * max_skin_height;
+
+        }
+
+        public void compute()
+        {
+            if(variety) { multiple_skin(); }
+            else { single_skin(); }
+        }
+
+        public void single_skin()
+        {
+            this.skin.Add(new Rectangle3d(Plane.WorldXY, this.max_skin_width, this.max_skin_height));
+        }
+        public void multiple_skin()
+        {
+            for(int skin_width = 0; skin_width <= this.max_skin_width; skin_width++)
+            {
+                for(int skin_height = 0; skin_height <= this.max_skin_height; skin_height++)
+                {
+                    if(skin_width * skin_height >= this.area * (1.0 - this.deviation))
+                    {
+                        this.skin.Add(new Rectangle3d(Plane.WorldXY, skin_width, skin_height));
+                    }
+                }
+            }
+        }
+    }
+}
